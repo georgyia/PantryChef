cdeb6bf5ec9b31a691f27fc6e8dbc522bd1916d9
georgychomakhashvili <ge95kan@mytum.de>
2023-11-19 01:40:44 +0100

    Initial Commit

---
+++ PantryChef/PantryChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryChefApp.swift
+//  PantryChef
+//
+//  Created by Georgy Chomakhashvili on 19.11.23.
+//
+
+import SwiftUI
+
+@main
+struct PantryChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
